f8dec0369778ba911a7a0d0592c1dd24ead4fd65
Veritas93 <loli93@yandex.ru>
2024-06-17 11:34:57 +0300

    initial commit

---
+++ src/Todolist.tsx
@@ -0,0 +1,40 @@
+import { TaskType } from "./App";
+import { Button } from "./Button";
+
+export type TodolistPropsType = {
+  title: string;
+  tasks: TaskType[];
+  date?: string;
+};
+
+export const Todolist = ({ title, tasks, date }: TodolistPropsType) => {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <div>
+        <input />
+        <Button title="+" />
+      </div>
+      {tasks.length === 0 ? (
+        <p>Тасок нет</p>
+      ) : (
+        <ul>
+          {tasks.map((task) => {
+            return (
+              <li key={task.id}>
+                <input type="checkbox" checked={task.isDone} />
+                <span>{task.title}</span>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      <div>
+        <Button title="All" />
+        <Button title="Active" />
+        <Button title="Completed" />
+      </div>
+      <div>{date}</div>
+    </div>
+  );
+};
